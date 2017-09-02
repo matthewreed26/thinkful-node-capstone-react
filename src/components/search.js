322dda760dc59fa5b5bd62b61ebd76eff0ee12cc
matthewreed26 <matthewreed26@gmail.com>
2017-09-02 12:22:04 -0400

    modified to one addAcronym in acronym-finder

--- src/components/search.js
+++ src/components/search.js
@@ -2,7 +2,6 @@ import React from 'react';
 //import {Link} from 'react-router-dom';
 //import './email-list.css';
 import {AcronymList} from './acronym-list';
-import {ModifyAcronym} from './modify-acronym';
 
 export function Search(props) {
     return (
@@ -11,18 +10,9 @@ export function Search(props) {
   				<input type='text' onChange={(event)=>{
   					props.trackFinderChanges(event.target.value);
   				}}/>
-          {props.finderResults.length?(
-            props.isEditing?
-              <ModifyAcronym
-                acronymChangesVal={props.acronymChangesVal}
-                trackAcronymChanges={(value)=>props.trackAcronymChanges(value)}
-                trackDefinitionChanges={(value)=>props.trackDefinitionChanges(value)}
-                definitionChangesVal={props.definitionChangesVal}
-                setEditing={()=>props.setEditing(null, false)}
-                saveChanges={()=>props.saveChanges()} />
-              :<AcronymList acronyms={props.finderResults}
-                setEditing={(acronym)=>props.setEditing(acronym, true)} />
-            )
+          {props.finderResults.length?
+            <AcronymList acronyms={props.finderResults}
+                setEditing={(acronym)=>props.setEditing(acronym)} />
             :''}
         </div>
     );
